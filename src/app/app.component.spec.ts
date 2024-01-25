812fc74b66ee2eeeb9c299d2740ebe10f650bfde
Betania Gonzalez <betaniagonz@gmail.com>
2024-01-25 18:51:12 -0500

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,66 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 
-describe('AppComponent', () => {
+describe('(1) test del componente principal', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
+    imports: [RouterTestingModule, ReactiveFormsModule, FormsModule],
     declarations: [AppComponent]
-  }));
+  }).compileComponents());
 
-  it('should create the app', () => {
+  it('Debe crear el componente', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'app-testing'`, () => {
+  it(`Debe tener el titulo: 'app-testing'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('app-testing');
   });
 
-  it('should render title', () => {
+  it('Debe retornar formulario inválido', () =>{
     const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('app-testing app is running!');
-  });
+
+    const form = app.form;
+    const email = app.form.controls['email'];
+    email.setValue('example@gmail.com');
+
+    expect(form.invalid).toBeTrue();
+  })
+
+  it('Debe retornar formulario válido', () =>{
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    fixture.detectChanges();
+
+    const email = app.form.controls['email'];
+    const password = app.form.controls['password'];
+
+    email.setValue('example@gmail.com');
+    password.setValue('123456')
+
+    expect(app.form.invalid).toBeFalse();
+  })
+
+  it('Debe de actualizar datos de usuario', () =>{
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    fixture.detectChanges();
+
+    const email = app.form.controls['email'];
+    const password = app.form.controls['password'];
+    email.setValue('example@gmail.com');
+    password.setValue('123456');
+
+    const btnElement = fixture.debugElement.query(By.css('button.btn'));
+    btnElement.nativeElement.click();
+    const testData = { user: 1 };
+    expect(app.isCheck).toEqual(testData);
+  })
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  private fb = inject(FormBuilder);
+  showEmoji: boolean = false;
   title = 'app-testing';
+  subTitle = 'Estamos creando este ejercicio para comenzar a aprender sobre pruebas unitarias en componentes'
+  contentEmoji = ''
+  dataSession: any;
+  form: FormGroup = new FormGroup({})
+  isCheck: any;
+  checkHuman: Array<any> = []
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    })
+  }
+
+  sendLogin(): void {
+    this.isCheck = { user: 1 }
+  }
 }
